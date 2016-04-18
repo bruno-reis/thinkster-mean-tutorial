44ef77b6579d4ad81bb89a7ea8c09a77a878eaf1
Bruno <bruno.reis@useboom.net>
2016-04-18 10:34:18 -0300

    added JWT authentication with passport

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ var bodyParser = require('body-parser');
 var app = express();
 
 var mongoose = require('mongoose');
+var passport = require('passport');
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
+require('./config/passport');
 mongoose.connect('mongodb://localhost/news');
 
 var routes = require('./routes/index');
@@ -29,6 +32,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(passport.initialize());
 app.use('/', routes);
 app.use('/users', users);
 
